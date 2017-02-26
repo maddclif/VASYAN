d9d08925cf3abe1f05891e1588464817669df1de
maddclif <maddclif@mail.ru>
2017-02-26 17:34:42 +0900

    rsg

--- ex1_4/main.c
+++ ex1_4/main.c
@@ -4,10 +4,11 @@ int main(void)
     float celsius, fahr;
     int lower = 0, upper = 300, step = 20;
     celsius = lower;
-    printf("    Table\n");
+    printf("Celsius  Fahr\n");
+    printf("-------------\n");
     while (celsius <= upper){
         fahr = (celsius * 9 / 5 +32);
-        printf ("%3.0f %6.1f\n", celsius, fahr);
+        printf ("%3.0f %9.1f\n", celsius, fahr);
         celsius = celsius + step;
     }
     return 0;
---
+++ ex1_5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int fahr;
+    printf ("Fahr Celsius\n");
+    printf ("------------\n");
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf ("%3d %6.1f\n", fahr, (5.0 / 9.0)*(fahr - 32));
+return 0;
+
+}
