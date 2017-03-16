d7efac704b977af30d1b9b44b832e42bcdfff60b
maddclif <maddclif@mail.ru>
2017-03-16 20:09:01 +0900

    kak ti u4il

--- ex1_10/main.c
+++ ex1_10/main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 int main(void)
 {
-    int c;
+    int c, flag;
     while ((c = getchar()) != EOF)
     {
+        flag = 0;
         if (c == '\t')
-            printf ("\\\\t");
-        if (c == '\b')
-            printf ("\\\\b");
-        if (c == '\\')
+        {
+            printf ("\\t");
+            flag = 1;
+        }
+         if (c == '\\')
+         {
             printf ("\\\\");
-        else putchar(c);
+            flag = 1;
+         }
+         if (c == '\b')
+         {
+            printf ("\\b");
+            flag = 1;
+         }
+        if(flag == 0)
+            putchar(c);
     }
     return 0;
 }
